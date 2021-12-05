281a2d43a1f064e0b241f526707e20d2a5c1ace0
rpsejz12 <rpsejz12@naver.com>
2021-12-05 21:53:27 +0900

    login 페이지

--- demo/src/main/java/com/example/demo/MainController.java
+++ demo/src/main/java/com/example/demo/MainController.java
@@ -1,10 +1,17 @@
 package com.example.demo;
 
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+@Controller
 public class MainController {
-	 @RequestMapping(value="/")
-	    public String main() {
-	        return "index";
-	    }
+	@RequestMapping(value="/")
+    public String main() {
+		 return "index";
+    }
+	
+	@RequestMapping("/login.do")
+    public String login() {
+        return "login";
+    }
 }
